2d4dd3562d6d20d28ef951cb5ffc57a3093a8d8e
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-08-09 10:45:03 +0530

    Create Count_subarray.cpp

---
+++ Count_subarray.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<" "<<endl;
+    int c=0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i;j<n;j++)
+        {
+            for(int k=i;k<=j;k++)
+            {
+                cout<<a[k]<<" ";
+            }
+            c++;
+            cout<<endl;
+        }
+    }
+    cout<<c+1;
+    return 0;
+}
